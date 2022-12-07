2fde5b7f971acfcf55b71e58f9190e6e0925adca
amc1999 <amc1999@gmail.com>
2022-12-07 11:58:23 -0500

    remove obsolete versions lock

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 // babel.config.js
 module.exports = {
+  plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining'
+  ],
   presets: [
     '@quasar/babel-preset-app',
     '@babel/preset-react'
